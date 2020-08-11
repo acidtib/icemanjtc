bf4a9b5aed9d8700e821caf687094f25774635ed
Dainel Vera <arubinofaux@gmail.com>
2020-08-10 21:50:47 -0400

    init

---
+++ assets/js/app.js
@@ -0,0 +1,45 @@
+(function() {
+
+  const apiInstance = axios.create({
+    baseURL: 'http://localhost:3000'
+  });
+
+  apiInstance.get('/stream')
+    .then(function (response) {
+      let data = response.data
+      let streamStatus = $(".stream-status")
+
+      if (data.type == "live") {
+        streamStatus.html(`<span class="badge bg-success">ONLINE</span>`)
+      } else {
+        streamStatus.html(`<span class="badge bg-dark">OFFLINE</span>`)
+      }
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+
+  apiInstance.get('/videos')
+    .then(function (response) {
+      let data = response.data.slice(0, 6)
+      let pastStreams = $(".past-streams")
+
+      $.each(data, function( key, video ) {
+        pastStreams.append(`
+          <div class="col-md-4 single-stream">
+            <a href="`+ video.url +`" target="_blank">
+              <div class="video-thumbnail">
+                <img class="mx-auto d-block" src="`+ video.thumbnail +`" alt="`+ video.title +`">
+              </div>
+            </a>
+          </div>
+        `)
+      });
+
+      $(".past-stream-button").html("VIEW MORE STREAMS")
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+
+})();
